7b3bd3521a16ceb6af5cb6191823791a029646d7
sparks <spmutugi@gmail.com>
2023-11-28 21:58:37 +0300

    edges

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -1,5 +1,4 @@
-
-word = "Holberton"
+word = "School"
 print("First 3 letters: {}".format(word[0:3]))
-print("Last 2 letters: {}".format(word[7:]))
-print("Middle word: {}".format(word[1:8]))
+print("Last 2 letters: {}".format(word[4:]))
+print("Middle word: {}".format(word[1:5]))
