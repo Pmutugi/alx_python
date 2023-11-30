bac4ab8bc13ecfbd1bb4c0a4376ca253d89cedb8
sparks <spmutugi@gmail.com>
2023-11-30 18:34:31 +0300

    fib

--- python-functions/4-fibonacci.py
+++ python-functions/4-fibonacci.py
@@ -1,6 +1,6 @@
 def fibonacci_sequence(n):
     sequence = [0,1]
-    for i in range(i,n):
+    for i in range(n,n):
         next_number = sequence[i-1] + sequence[i-2]
         sequence.append(next_number)
         return sequence
