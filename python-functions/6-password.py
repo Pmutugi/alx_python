ca0e6b2b293a1eace4d924ad4f88bcdb677da505
sparks <spmutugi@gmail.com>
2023-11-30 18:53:24 +0300

    password

---
+++ python-functions/6-password.py
@@ -0,0 +1,16 @@
+def validate_password(password):
+  
+    if len(password) < 8:
+        return False
+
+    has_uppercase = any(char.isupper() for char in password)
+    has_lowercase = any(char.islower() for char in password)
+    has_digit = any(char.isdigit() for char in password)
+
+    if not (has_uppercase and has_lowercase and has_digit):
+        return False
+
+    if ' ' in password:
+        return False
+
+    return True
