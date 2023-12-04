46a4d07ac2f82478509bfd932aad10600c00dbe8
sparks <spmutugi@gmail.com>
2023-12-04 22:57:12 +0300

    arguments

---
+++ python-import_modules/1-args.py
@@ -0,0 +1,19 @@
+import sys
+def printarguments():
+    arguments = ["philluip", "john", "adrian"]
+    #check number of arguments
+    num_arguments = len(arguments)
+     
+    output = ""
+    
+    #conditional statements
+    if num_arguments == 0:
+        output += "0 argument{} \n".format('s' if num_arguments !=1 else "")
+    else:
+        output +="{} argument{} \n".format(num_arguments, 's' if num_arguments !=1 else "")
+        
+    #to number our arguments
+    for i, j in enumerate(arguments, 1):
+        output +="{}: {} \n".format(i, j)
+    return output   
+print(printarguments()) 
