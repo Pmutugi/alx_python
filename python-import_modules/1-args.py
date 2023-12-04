16ddcb95b8d11cc97c31cf25a83d990778f11ae4
sparks <spmutugi@gmail.com>
2023-12-04 23:51:34 +0300

    arg

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -8,14 +8,16 @@ def printarguments():
     
     #conditional statements
     if num_arguments == 0:
-        output +="0 argument{}.\n".format('s' if num_arguments !=1 else "")
+        output += "0 argument{}.\n".format('s' if num_arguments != 1 else '')
     else:
-        output +="{} argument{}:\n".format(num_arguments,'s' if num_arguments !=1 else "")
-        
-    #to number our arguments
-    for i, j in enumerate(arguments, 1):
-        output +="{}: {}\n".format(i, j)
+        output += "{} argument{}:\n".format(num_arguments, 's' if num_arguments != 1 else '')
+
+        # Construct string for each argument with its position
+    for i, arg in enumerate(arguments, 1):
+         output += "{}: {}\n".format(i, arg)
     return output   
 if __name__== '__main__':
 
-    print(printarguments()) 
+     result = printarguments()
+     if result:
+        print(result)
