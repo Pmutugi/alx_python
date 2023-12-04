f4cedf9f2df6c3add4690fc619637fbe12013896
sparks <spmutugi@gmail.com>
2023-12-04 14:47:41 +0300

    UPDATES

--- python-hello_world/test.py
+++ python-hello_world/test.py
@@ -21,3 +21,17 @@ elif number<0:
          print("Last digit of",number,"is",last,"and is 0")
     else:
          print("Last digit of",number, "is",last, "and is 0")
+         
+         
+def add(a, b):
+    sum = a + b
+    return sum 
+
+if __name__== "__main__":
+    a = 1
+    b = 2
+from add_0 import add
+results= add(a, b)
+print("{}+{}={}".format(a,b,results))
+
+
