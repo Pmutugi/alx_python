1a2935e7f37fe45abbbe5b76ca7202b3f9fe72de
sparks <spmutugi@gmail.com>
2023-12-06 23:08:21 +0300

    returns

--- python-data_structures/2-multiple_returns.py
+++ python-data_structures/2-multiple_returns.py
@@ -1,8 +1,9 @@
 def multiple_returns(sentence):
     length = len(sentence)
-    if length== 0:
-        return None
-    else:
-        for i in sentence:
-            print ("Length: {:d} - First character: {}".format(length, sentence[0]))
-        
+    first = sentence[0] if length > 0 else None
+    return length, first
+if __name__=="__main__":
+         
+    sentence = "At Holberton school, I learnt C!"
+    l, f = multiple_returns(sentence)
+    print("Length: {:d} - First character: {}".format(l, f))
