3070464c71fd22f181271fb59ff731018d2b2418
sparks <spmutugi@gmail.com>
2023-12-06 22:04:33 +0300

    arg

--- python-data_structures/2-multiple_returns.py
+++ python-data_structures/2-multiple_returns.py
@@ -1,9 +1,8 @@
 def multiple_returns(sentence):
     length = len(sentence)
-    first = sentence([0][0])
-    if len(sentence)=="":
+    if length== 0:
         return None
     else:
         for i in sentence:
-            print("Length: {:d} - First character: {}".format(length, first))
+            print ("Length: {:d} - First character: {}".format(length, sentence[0]))
         
