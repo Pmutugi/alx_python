5c2033d802fd0a9df2d6d17214cb8b9b1c27cbaf
sparks <spmutugi@gmail.com>
2023-12-06 14:24:20 +0300

    character

---
+++ python-data_structures/2-multiple_returns.py
@@ -0,0 +1,10 @@
+def multiple_returns(sentence):
+    if sentence == "":
+        return None
+    else:
+        sentence = ''
+        for i in sentence:
+            length = len(sentence)
+            first = sentence([0][0])
+    print("Length: {:d} - First character: {}".format(length, first))
+        
