d96c9e9f5001da8298a3fac25138dbd0d0c70ca3
sparks <spmutugi@gmail.com>
2023-12-07 18:31:19 +0300

    key

--- python-more_data_structures/2-update_dictionary.py
+++ python-more_data_structures/2-update_dictionary.py
@@ -3,14 +3,3 @@ def update_dictionary(a_dictionary, key, value):
     # Otherwise, add a new key/value pair
     a_dictionary[key] = value
 
-# Example usage:
-my_dictionary = {'a': 1, 'b': 2, 'c': 3}
-
-# Updating existing key 'b'
-update_dictionary(my_dictionary, 'b', 10)
-
-# Adding a new key 'd'
-update_dictionary(my_dictionary, 'd', 4)
-
-# Display the updated dictionary
-print(my_dictionary)
