1c5e3555ba9446e42ff986f721d18ac679b5d786
sparks <spmutugi@gmail.com>
2023-12-12 21:45:40 +0300

    key

--- python-more_data_structures/2-update_dictionary.py
+++ python-more_data_structures/2-update_dictionary.py
@@ -1,27 +1,7 @@
-def update_dictionary(a_dictionary, key, value):
-    key = ""
-    if key in a_dictionary:
-        key.replace(a_dictionary, key)
-    else:
-        
-        
-# def print_sorted_dictionary(my_dict):
-#     """ Print sorted dictionary """
-#     keys = sorted(my_dict.keys())
-#     for k in keys:
-#         print("{}: {}".format(k, my_dict[k]))
-
-# a_dictionary = { 'language': "C", 'number': 89, 'track': "Low level" }
-# new_dict = update_dictionary(a_dictionary, 'language', "Python")
-# print_sorted_dictionary(new_dict)
-# print("--")
-# print_sorted_dictionary(a_dictionary)
-
-# print("--")
-# print("--")
-
-# new_dict = update_dictionary(a_dictionary, 'city', "San Francisco")
-# print_sorted_dictionary(new_dict)
-# print("--")
-# print_sorted_dictionary(a_dictionary)
+def update_and_print_sorted_dict(a_dictionary, key, value):
+    a_dictionary[key] = value
+    sorted_keys = sorted(a_dictionary.keys())
+    
+    for k in sorted_keys:
+        print("{}: {}".format(k, a_dictionary[k]))
         
