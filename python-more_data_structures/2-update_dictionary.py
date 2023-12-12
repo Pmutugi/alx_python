6c62fa17838ab4f4994f0661511f5c35f1d0746c
sparks <spmutugi@gmail.com>
2023-12-12 22:16:59 +0300

    update dict

--- python-more_data_structures/2-update_dictionary.py
+++ python-more_data_structures/2-update_dictionary.py
@@ -1,7 +1,8 @@
 def update_and_print_sorted_dict(a_dictionary, key, value):
-    a_dictionary[key] = value
-    sorted_keys = sorted(a_dictionary.keys())
-    
-    for k in sorted_keys:
-        print("{}: {}".format(k, a_dictionary[k]))
-        
+     a_dictionary[key] = value
+     return a_dictionary
+def print_sorted_dictionary(my_dict):
+     '''Print sorted dictionary'''
+     keys =sorted(my_dict)
+     for k in keys:
+         print("{}: {}".format(k, my_dict[k]))
