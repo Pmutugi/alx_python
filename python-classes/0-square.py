afe88cd2dc200c7ec2ec98ff5cfbee9abb896396
sparks <spmutugi@gmail.com>
2023-12-12 23:42:53 +0300

    square

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -7,17 +7,16 @@ def __init__(self, size):
     self.__size = size
     
 if __name__=="__main__":
-    my_square = Square(3)
-    '''try and exception method'''
+   my_square = Square(3)
+print(type(my_square))
+print(my_square.__dict__)
 
-    print(type(my_square))
-    print(my_square.__dict__)
-    try:
-         print(my_square.size)
-    except Exception as e:
-         print(e)
+try:
+    print(my_square.size)
+except Exception as e:
+    print(e)
 
-    try:
-        print(my_square.__size)
-    except Exception as e:
-        print(e)
+try:
+    print(my_square.__size)
+except Exception as e:
+    print(e)
