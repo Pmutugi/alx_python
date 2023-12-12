6a06aa87df349b3f28aa8e4368f1b3f3866fd042
sparks <spmutugi@gmail.com>
2023-12-12 23:16:25 +0300

    push

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,13 +1,13 @@
-class my_square:
-    """this is a class of my_square.
+"""this is a class of my_square.
     It does nothing hence the pass function .
-    """
+"""
+class my_square:
     pass
-def __init__(self, size):
-    """
+    """below is a module to initialize size.
     the __ underscore are used to make the attribute private
     this means they cannot be accessed.
     """
+def __init__(self, size):
     self.__size = size
     
 if __name__=="__main__":
