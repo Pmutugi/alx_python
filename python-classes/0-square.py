c826d5bd3f71b414bb6b96c7233858fae8d7d5f1
sparks <spmutugi@gmail.com>
2023-12-12 23:36:49 +0300

    square

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -2,14 +2,13 @@
     It does nothing hence the pass function .
 """
 class my_square:
-    """ To initialize size.
-    the __ underscore are used to make the attribute private
-    this means they cannot be accessed.
-    """
+    '''initializing size'''
 def __init__(self, size):
     self.__size = size
     
 if __name__=="__main__":
+    '''try and exception method'''
+
     print(type(my_square))
     print(my_square.__dict__)
     try:
