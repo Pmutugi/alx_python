a1c117141ec9cc13761aae14c693e9436c25cda4
sparks <spmutugi@gmail.com>
2023-12-12 22:31:19 +0300

    best

--- python-more_data_structures/3-best_score.py
+++ python-more_data_structures/3-best_score.py
@@ -8,11 +8,6 @@ def best_score(a_dictionary):
         # Return None if the dictionary is empty
         return None
     
-a_dictionary = {'John': 12, 'Bob': 14, 'Mike': 14, 'Molly': 16, 'Adam': 10}
-best_key = best_score(a_dictionary)
-print("Best score: {}".format(best_key))
 
-best_key = best_score(None)
-print("Best score: {}".format(best_key))
 
 
