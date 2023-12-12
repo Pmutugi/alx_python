5bd25416ccd69b0796b697ef9acae28a894fbc96
sparks <spmutugi@gmail.com>
2023-12-13 00:34:13 +0300

    square

---
+++ python-classes/2-square.py
@@ -0,0 +1,30 @@
+"""this is a class of my_square.
+    It does nothing hence the pass function .
+"""
+class Square:
+    '''initializing size'''
+    def __init__(self, size=0):
+        if not isinstance(size,int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+            
+    def area(self):
+        '''we are now calculating area of a square'''
+        my_square_1 = Square(3)
+        print("Area: {}".format(my_square_1.area()))
+
+        try:
+            print(my_square_1.size)
+        except Exception as e:
+            print(e)
+
+        try:
+            print(my_square_1.__size)
+        except Exception as e:
+            print(e)
+
+        my_square_2 = Square(5)
+        print("Area: {}".format(my_square_2.area()))
