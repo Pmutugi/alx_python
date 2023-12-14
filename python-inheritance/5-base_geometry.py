ae7162cbb392a0b1d41a2e73b45b1250e943f3f0
sparks <spmutugi@gmail.com>
2023-12-14 13:11:38 +0300

    value error

---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,14 @@
+'''This is a class that does geometry class'''
+class BaseGeometry:
+    '''this class checks conditionsnothing'''
+    def area(self): 
+        raise Exception("area() is not implemented")
+    def integer_validator(self, name, value):
+        if not isinstance(value,int):
+            raise TypeError("name must be an integer")
+        
+        '''value less than zero'''
+        if value <=0:
+            raise ValueError('<name> must be greater than 0')
+        
+        
