04b501ff6fc10a808c9128d00d92faa0b203a70a
sparks <spmutugi@gmail.com>
2023-12-14 14:02:48 +0300

    area

--- python-inheritance/7-rectangle.py
+++ python-inheritance/7-rectangle.py
@@ -1,10 +1,14 @@
+'''importing base geometry'''
 BaseGeometry = __import__('5-base_geometry').BaseGeometry
+'''This is the class of a rectangle'''
 class Rectangle(BaseGeometry):
+    '''initializing the class width and height'''
     def __init__(self, width, height):
         self.__width = width
         self.__height = height
         self.integer_validator("width",width)
         self.integer_validator ("height",height)
+        '''defining area function'''
     def area(self):
         '''calculate the area'''
         return self.__width * self.__height
