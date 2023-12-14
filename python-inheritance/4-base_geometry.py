f6b5265398c85546a25f2350ef29ec3f2ddc0be3
sparks <spmutugi@gmail.com>
2023-12-14 12:50:44 +0300

    area

--- python-inheritance/4-base_geometry.py
+++ python-inheritance/4-base_geometry.py
@@ -1,7 +1,6 @@
 '''This is an empty class'''
 class BaseGeometry:
     '''this class does nothing'''
-    def __init__(self, area): 
-        self.area =area
+    def __init__(self,): 
         raise Exception("area() is not implemented")
-    
+
