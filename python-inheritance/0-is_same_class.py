d1c8ae0b290a2ff6651ddce0b87ab206447c5e1d
sparks <spmutugi@gmail.com>
2023-12-14 12:11:35 +0300

    true or false

--- python-inheritance/0-is_same_class.py
+++ python-inheritance/0-is_same_class.py
@@ -1,22 +1,4 @@
-'''this is the main class'''
-class a:
-    # a = 1
-    '''class variables goes i here'''
-    def __init__(self, a):
-        self.a = a
-    def is_same_class(obj, a_class):
-        a = 1
-    if is_same_class(a, int):
-        print("{} is an instance of the class {}".format(a, int.__name__))
-    if is_same_class(a, float):
-        print("{} is an instance of the class {}".format(a, float.__name__))
-    if is_same_class(a, object):
-        print("{} is an instance of the class {}".format(a, object.__name__))
-
-        # if obj in a_class:
-        #     return True
-        # else:
-        #     return False
-        
-# if __name__=="__main__":
-    
+'''define the module '''
+def is_same_class(obj, a_class):
+    '''to check if its true or false'''
+    return type(obj) == a_class
