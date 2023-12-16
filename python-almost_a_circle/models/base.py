1ce8c72c1fe99d2679904c25daeed8c61280bdb3
sparks <spmutugi@gmail.com>
2023-12-17 00:32:07 +0300

    pushh

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -2,12 +2,12 @@
 class Base:
     '''the private class instance goes in here'''
     __nb_objects = 0
-def __init__(self, id=None):
-     '''initializing the none function'''
-     if id is not None:
-        self.id = id
-     else:
-       '''comparing the function'''
-       Base.__nb_objects +=1
-       self.id = Base.__nb_objects
+    def __init__(self, id=None):
+        '''initializing the none function'''
+        if id is not None:
+            self.id = id
+        else:
+         '''comparing the function'''
+        Base.__nb_objects +=1
+        self.id = Base.__nb_objects
 
