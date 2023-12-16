3d41d19c6a297475c16e9582df3501a3acabaeea
sparks <spmutugi@gmail.com>
2023-12-17 00:26:34 +0300

    push

---
+++ python-almost_a_circle/README.md
@@ -0,0 +1,1 @@
+my almost a circle python
--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -3,24 +3,11 @@ class Base:
     '''the private class instance goes in here'''
     __nb_objects = 0
 def __init__(self, id=None):
+     '''initializing the none function'''
      if id == None:
-         __nb_objects = (id)
+        self.id = id
      else:
-        __nb_objects +=1
+       '''comparing the function'''
+       Base.__nb_objects +=1
+       self.id = Base.__nb_objects
 
-if __name__ == "__main__":
-
-    b1 = Base()
-    print(b1.id)
-
-    b2 = Base()
-    print(b2.id)
-
-    b3 = Base()
-    print(b3.id)
-
-    b4 = Base(12)
-    print(b4.id)
-
-    b5 = Base()
-    print(b5.id)
