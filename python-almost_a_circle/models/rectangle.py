55daf3b8739649ff413d020a9178af590fb244e1
sparks <spmutugi@gmail.com>
2023-12-18 12:44:04 +0300

    fgn

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -37,9 +37,9 @@ class Rectangle(Base):
     @x.setter
     def x(self, value):
         if not isinstance(value,int):
-            raise TypeError("x must be an integer")
+            raise TypeError ("x must be an integer")
         elif value < 0:
-            raise ValueError("x must be >= 0")
+            raise ValueError ("x must be >= 0")
         else:
          self.__x= value
     @property
@@ -49,7 +49,7 @@ class Rectangle(Base):
     def y(self, value):
         if not isinstance(value,int):
             raise TypeError("y must be an integer")
-        elif value < 0:
+        elif value <= 0:
             raise ValueError("y must be >= 0")
         else:
            self.__y= value
