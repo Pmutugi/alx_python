ba1cf0d6e3bc6db54e7149d59c96d246d19ba706
sparks <spmutugi@gmail.com>
2023-12-18 09:56:42 +0300

    formart setters

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -15,23 +15,66 @@ class Rectangle(Base):
         return self.__width
     @width.setter
     def width(self, width):
+         if not isinstance(width,int):
+            raise TypeError("width must be an integer")
+         elif width < 0:
+            raise ValueError("width must be > 0")
+         else:
             self.__width = width  
     @property
     def height(self):
         return self.__height
     @height.setter
     def height(self, height):
-              self.__height= height
+        if not isinstance(height,int):
+            raise TypeError("height must be an integer")
+        elif height < 0:
+            raise ValueError("height must be > 0")
+        else:
+         self.__height= height
     @property
     def x(self):
         return self.__x
     @x.setter
     def x(self, x):
+        if not isinstance(x,int):
+            raise TypeError("x must be an integer")
+        elif x <= 0:
+            raise ValueError("x must be >= 0")
+        else:
          self.__x= x
     @property
     def y(self):
         return self.__y
     @y.setter
     def y(self, y):
-        self.__y= y
+        if not isinstance(y,int):
+            raise TypeError("y must be an integer")
+        elif y <= 0:
+            raise ValueError("y must be >= 0")
+        else:
+           self.__y= y
+if __name__ == "__main__":
+
+    try:
+        Rectangle(10, "2")
+    except Exception as e:
+        print("[{}] {}".format(e.__class__.__name__, e))
+
+    try:
+        r = Rectangle(10, 2)
+        r.width = -10
+    except Exception as e:
+        print("[{}] {}".format(e.__class__.__name__, e))
+
+    try:
+        r = Rectangle(10, 2)
+        r.x = {}
+    except Exception as e:
+        print("[{}] {}".format(e.__class__.__name__, e))
+
+    try:
+        Rectangle(10, 2, 3, -1)
+    except Exception as e:
+        print("[{}] {}".format(e.__class__.__name__, e))
 
