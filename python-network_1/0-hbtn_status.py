b1a638308dce81c41e5c757dc944352cd8f3bf2e
sparks <spmutugi@gmail.com>
2023-12-19 20:59:26 +0300

    requests

---
+++ python-network_1/0-hbtn_status.py
@@ -0,0 +1,7 @@
+'''import requests packages installed in my local pc'''
+import requests
+'''define url as a variable that contains the actual url'''
+url= 'https://alu-intranet.hbtn.io/status'
+'''using the get method to get the url requests'''
+response = requests.get(url)
+print(response)
