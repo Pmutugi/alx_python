229b0f888de4d258714b9da502c739a6fe6a3b60
sparks <spmutugi@gmail.com>
2023-12-21 01:04:39 +0300

    post method

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -3,7 +3,7 @@ import sys
 import requests
 
 url = input('Enter URL: ') if len(sys.argv) < 2 else sys.argv[1]
-email = url = input('Enter URL: ') if len(sys.argv) < 3 else sys.argv[2]
+email = url = input('Enter Email: ') if len(sys.argv) < 3 else sys.argv[2]
 
 '''now set up the pay-load, the data to be sent over the url'''
 data_to_send = {
