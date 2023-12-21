46313079a0c2919edd45030bf119f14ae0cfef88
sparks <spmutugi@gmail.com>
2023-12-21 15:59:16 +0300

    vlidation letter

--- python-network_1/5-json_api.py
+++ python-network_1/5-json_api.py
@@ -2,7 +2,7 @@ import sys
 import requests
 ''' imports from the modules'''
 url = "http://0.0.0.0:5000/search_user"
-q = "" if len(sys.argv) < 1 else sys.argv[1]
+q = " " if len(sys.argv) < 1 else sys.argv[1]
 '''send data to the url'''
 letter ={
     'q':q
