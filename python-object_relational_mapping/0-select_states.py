864b9179f8f07ee6c0857d2d9eb92d043f779e06
sparks <spmutugi@gmail.com>
2024-01-15 22:51:50 +0300

    connection

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -2,8 +2,8 @@ import MySQLdb
 from sys import argv
 mysql_username = argv[1]
 mysql_pass = argv[2]
-my_db =argv[3]
-my_conn= MySQLdb.connector(host='localhost',port='3306',user=mysql_username,passwd= mysql_pass,db =my_db)
+my_db = argv[3]
+my_conn= MySQLdb.connector(host='localhost',port='3306',user=mysql_username,passwd= mysql_pass,database = my_db)
 cursor = my_conn.cursor()
 query= 'SELECT * FROM states ORDER BY id ASC'
 cursor.execute(query)
