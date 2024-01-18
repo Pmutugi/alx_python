b7f4b7c3c77067aa5c31b072df6d765fa9a0afad
sparks <spmutugi@gmail.com>
2024-01-18 10:59:01 +0300

    updates

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -6,9 +6,9 @@ if __name__=='__main__':
     my_db = argv[3]
     my_conn= MySQLdb.connect(host='localhost',port=3306,user=mysql_username,passwd= mysql_pass,db = my_db)
     cursor = my_conn.cursor()
-    cursor.execute('SELECT * FROM states WHERE BINARY LEFT(name, 1) = "N" ORDER BY id ASC')
+    cursor.execute('SELECT * FROM states WHERE BINARY LEFT (name, 1) = "N" ORDER BY id ASC')
     rows = cursor.fetchall()
     for row in rows:
-        print (rows)
+        print (row)
     cursor.close()
     my_conn.close()
