5d2ae5af7648c29c7c802e87c2676454312594f5
sparks <spmutugi@gmail.com>
2024-01-18 20:17:54 +0300

    import sqlalchemy

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -3,8 +3,9 @@ from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
+'''class states'''
 class States(Base):
-    '''state class'''
+    '''state class setting from the imports'''
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True)
     name =  Column(String(128), nullable=False) 
