99a46edb8caa374e0a42d007d6514375f589dea3
sparks <spmutugi@gmail.com>
2024-01-23 23:17:43 +0300

    enroute

---
+++ python-web_framework/3-python_route.py
@@ -0,0 +1,18 @@
+'''WEB SERVER: dynamic routes in flask framework'''
+from flask import Flask
+app = Flask(__name__)
+
+        
+@app.route("/",strict_slashes=False)
+def Hello_HBNB():
+    return "Hello HBNB!"
+@app.route('/hbnb', strict_slashes=False)
+def HBNB():
+    return 'HBNB'
+@app.route("/c/<text>")
+def c_route (text):
+    formated = text.replace('_', ' ')
+    return f"C {formated}"
+
+if __name__=="__main__":
+    app.run(debug=True,host='0.0.0.0',port=5000)
