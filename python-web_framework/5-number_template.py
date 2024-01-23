2aae14e6fad96e73135a29ecf32c30d116151921
sparks <spmutugi@gmail.com>
2024-01-24 00:05:27 +0300

    templates

---
+++ python-web_framework/5-number_template.py
@@ -0,0 +1,31 @@
+'''WEB SERVER: dynamic routes in flask framework'''
+from flask import Flask , render_template
+app = Flask(__name__)
+
+        
+@app.route("/",strict_slashes=False)
+def Hello_HBNB():
+    return "Hello HBNB!"
+@app.route('/hbnb', strict_slashes=False)
+def HBNB():
+    return 'HBNB'
+@app.route("/c/<text>")
+def c_route (text):
+    formated = text.replace('_', ' ')
+    return f"C {formated}"
+@app.route("/python/<text>")
+@app.route('/python/')
+def python(text='is cool'):
+    formated = text.replace('_', ' ')
+    return  f"Python {formated}"
+@app.route('/number/<int:n>')
+def integer (n):
+    return f'{n} is a number'
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_template(n):
+    if isinstance(n, int):
+        return render_template('number_template.html', n=n)
+    else:
+        return "Not a valid number"
+if __name__=="__main__":
+    app.run(debug=True,host='0.0.0.0',port=5000)      
