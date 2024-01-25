3d2b8e2e8fad1846b4c23b38c9486a551d8afd7b
sparks <spmutugi@gmail.com>
2024-01-25 09:56:19 +0300

    odd or even

--- python-web_framework/6-number_odd_or_even.py
+++ python-web_framework/6-number_odd_or_even.py
@@ -24,10 +24,10 @@ def integer (n):
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template(n):
     return render_template('5-number.html', n=n)
-@app.route('/number_even_or_odd/<int:n>', strict_slashes=False)
+@app.route('/number_odd_or_even/<int:n>',strict_slashes=False)
 def number_odd_or_even(n):
-    odd_or_even = "even" if n % 2 == 0 else "odd"
-    return render_template('6-number_odd_or_even.html', n=n, odd_or_even=odd_or_even)
+    odd_even = "even" if n % 2 == 0 else "odd"
+    return render_template('6-number_odd_or_even.html', n=n, odd_even=odd_even)
 
    
 if __name__=="__main__":
