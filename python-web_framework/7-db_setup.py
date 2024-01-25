e801c40606026d989740036ee38725955252b0f9
sparks <spmutugi@gmail.com>
2024-01-25 11:22:45 +0300

    username set up and configuration

--- python-web_framework/7-db_setup.py
+++ python-web_framework/7-db_setup.py
@@ -29,7 +29,7 @@ class User(db.Model):
     email =  db.Column(db.String(120),unique=True,nullable=False)
 
 def __repr__(self):
-    return f"<User(self.username)"
+    return f"<User{self.username}>"
 #################################################################
 
 # Create the database tables
