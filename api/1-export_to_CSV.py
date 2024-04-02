b7126ff0e51aaa9e8650723c7e77e83244311f7f
sparks <spmutugi@gmail.com>
2024-04-02 09:56:18 +0300

    csv

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,28 +1,41 @@
+""" Using what you did in the task #0, extend your Python script to export
+data in the CSV format. """
 import csv
 import requests
-import sys
+from sys import argv
 
 
-url = "https://jsonplaceholder.typicode.com/users"
-user_id = sys.argv[1]
-endpoint1 = f"{url}/{user_id}"
-endpoint2 = f"{url}/{user_id}/todos"
+def export_to_CSV(sizeofReq):
+    """ The task define export to the CSV format"""
 
-response1 = requests.get(url=endpoint1)
-response2 = requests.get(url=endpoint2)
+    # Variables
+    allTasks = []
 
-data1 = response1.json()
-data2 = response2.json()
-myarray = []
-csvheader = ['USER_ID', 'USERNAME', 'TASK_COMPLETED_STATUS', 'TASK_TITLE']
+    link = "https://jsonplaceholder.typicode.com"
 
-for task in data2:  # Iterate over tasks from the second endpoint
-    listing = [task['userId'], data1['username'], task['completed'], task['title']]
-    myarray.append(listing)
+    # get requests
+    usersRes = requests.get("{}/users/{}".format(link, sizeofReq))
+    todosRes = requests.get("{}/users/{}/todos".format(link, sizeofReq))
+
+    # Get the json from responses
+    name = usersRes.json().get('username')
+    todosJson = todosRes.json()
+
+    # Save the employee Name -- Loop the tasks and save
+    for task in todosJson:
+        taskRow = []
+        taskRow.append(sizeofReq)
+        taskRow.append(name)
+        taskRow.append(task.get('completed'))
+        taskRow.append(task.get('title'))
+        allTasks.append(taskRow)
+
+    with open("{}.csv".format(sizeofReq), "w") as csvFile:
+        csvWriter = csv.writer(csvFile, quoting=csv.QUOTE_ALL)
+        csvWriter.writerows(allTasks)
+
+    return 0
 
 
-with open(f"{user_id}.csv", 'w', encoding='UTF8', newline='') as f:
-    writer = csv.writer(f)
-
-    writer.writerow(csvheader)
-    writer.writerows(myarray)
+if __name__ == '__main__':
+    export_to_CSV(int(argv[1]))
